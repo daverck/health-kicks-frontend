b1c8deec1cea4c12f898c2dd4f66c725f237b4c3
David Serck <d.serck@stag.technofuturtic.education>
2026-09-04 13:41:31 +0200

    feat(auth): add Azure Entra ID environment configurations

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,4 +2,11 @@ export const environment = {
   production: false,
   apiUrl: 'https://healthkicks.duckdns.org:8443',
   googleClientId: '84866353361-f0i371foa1q4sdvpdal29t6e8qfv483k.apps.googleusercontent.com',
+  azureClientId: '00000000-0000-0000-0000-000000000000',
+  azureTenantId: 'common',
+  azureRedirectUri: 'http://localhost:4200/login/callback',
+  AZURE_CLIENT_ID: '00000000-0000-0000-0000-000000000000',
+  AZURE_TENANT_ID: 'common',
+  AZURE_REDIRECT_URI: 'http://localhost:4200/login/callback',
 };
+
